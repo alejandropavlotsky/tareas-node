a0a87eff05757c64c8930e649f4ec912c1ef45ce
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-02-24 16:14:24 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+require('colors');
+const { mostrarMenu } = require('./helpers/mensajes.js');
+
+console.clear();
+
+const main = async () => {
+  console.log('Hola Mundo');
+  mostrarMenu();
+};
+main();
---
+++ helpers/mensajes.js
@@ -0,0 +1,20 @@
+require('colors');
+
+const mostrarMenu = async () => {
+  console.clear();
+  console.log('======================='.green);
+  console.log(' Seleccione una opción'.green);
+  console.log('=======================\n'.green);
+
+  console.log(`${'1.'.green} Crear tarea`);
+  console.log(`${'2.'.green} Listar tareas`);
+  console.log(`${'3.'.green} Listar tareas completadas`);
+  console.log(`${'4.'.green} Listar tareas pendientes`);
+  console.log(`${'5.'.green} Completar tareas`);
+  console.log(`${'6.'.green} Borrar tarea`);
+  console.log(`${'0.'.green} Salir\n`);
+};
+
+module.exports = {
+  mostrarMenu,
+};
