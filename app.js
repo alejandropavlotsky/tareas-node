1a30296252703a2c67f2710acc0cf65a49f8e02d
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-03-09 12:21:13 +0100

    ADD tareas

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 require('colors');
-const { inquirerMenu, pause } = require('./helpers/inquirer');
-console.clear();
+const { inquirerMenu, pause, leerInput } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
 
 const main = async () => {
-  console.log('Hola Mundo');
-
   let opt = '';
+  const tareas = new Tareas();
+
+  const tareasDB = leerDB();
+
+  if (tareasDB) {
+    tareas.cargarTareasFromArray(tareasDB);
+  }
+
   do {
     opt = await inquirerMenu();
+
+    switch (opt) {
+      case '1':
+        const desc = await leerInput('Descripción:');
+        tareas.crearTarea(desc);
+        break;
+      case '2':
+        tareas.listadoCompleto();
+        break;
+    }
+
+    guardarDB(tareas.listadoArr);
     await pause();
   } while (opt !== '0');
 };
